6df3cf9fe58b1b71f75c9d41f18c09a610dc2488
Thakshi20010921 <umthakshilakumari2001@gmail.com>
2025-08-23 20:41:20 +0530

    database function changes

--- backend/database.js
+++ backend/database.js
@@ -37,7 +37,7 @@ const readTeachers = async () => {
 }
 
 const readTeacherInfo = async (id) => {
-    const sql = `SELECT * FROM dummyData`
+    const sql = `SELECT * FROM teacher`
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql)
